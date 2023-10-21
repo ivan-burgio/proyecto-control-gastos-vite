65d8f6baff7132b851fda8a70ae6bf942de5dc2b
ivan-burgio <ivan.24.burgio@gmail.com>
2023-10-21 15:57:06 -0300

    Creando uanfuncion para generar un id unico y añadiendo gastos al state

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import Header from './components/Header';
-import IconoNuevoGasto from './img/nuevo-gasto.svg';
 import Modal from './components/Modal';
+import { generarId } from './helpers';
+import IconoNuevoGasto from './img/nuevo-gasto.svg';
 
 export default function App() {
     const [presupuesto, setPresupuesto] = useState(0);
@@ -21,7 +22,14 @@ export default function App() {
     }
 
     const guardarGasto = gasto => {
+        gasto.id = generarId();
+        setGastos([...gastos, gasto]);
 
+        setAnimarModal(false);
+
+        setTimeout(() => {
+            setModal(false);
+        }, 500);
     }
 
     return (
---
+++ src/helpers/index.js
@@ -0,0 +1,5 @@
+export const generarId = () => {
+    const random = Math.random().toString(36).substr(2);
+    const fecha = Date.now().toString(36);
+    return random + fecha;
+}
