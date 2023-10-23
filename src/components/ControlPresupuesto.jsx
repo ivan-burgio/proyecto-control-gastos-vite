50938635f770aeaaf7eb8af746e7c9f4b553c85f
ivan-burgio <ivan.24.burgio@gmail.com>
2023-10-23 11:51:32 -0300

    Calculando el presupuesto disponible

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -6,6 +6,9 @@ export default function ControlPresupuesto({presupuesto, gastos}) {
 
     useEffect(() => {
         const totalGastado = gastos.reduce((total, gasto) => gasto.cantidad + total, 0);
+        const totalDisponible = presupuesto - totalGastado;
+
+        setDisponible(totalDisponible);
         setGastado(totalGastado);
     }, [gastos])
 
