67283c11f68fe133cb86f21c4d31eb949b00ce67
ivan-burgio <ivan.24.burgio@gmail.com>
2023-10-25 11:38:22 -0300

    finalizando efecto del swiper

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -30,19 +30,27 @@ const diccionarioIconos = {
 export default function Gasto({gasto}) {
     const {categoria, nombre, cantidad, id, fecha} = gasto;
 
-    const leadingActions = () => {
+    const leadingActions = () => (
+        <LeadingActions>
+            <SwipeAction onClick={() => console.log('editar')}>
+                Editar
+            </SwipeAction>
+        </LeadingActions>
+    )
 
-    }
-
-    const trailingActions = () => {
-        
-    }
+    const trailingActions = () => (
+        <TrailingActions>
+            <SwipeAction onClick={() => console.log('eliminar')}>
+                Eliminar
+            </SwipeAction>
+        </TrailingActions>
+    )
 
     return (
         <SwipeableList>
             <SwipeableListItem
-                leadingActions = {leadingActions}
-                trailingActions = {trailingActions}
+                leadingActions = {leadingActions()}
+                trailingActions = {trailingActions()}
             >
                 <div className='gasto sombra'>
                     <div className="contenido-gasto">
