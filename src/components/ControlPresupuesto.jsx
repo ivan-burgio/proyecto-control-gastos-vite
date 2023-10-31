32c2fdb9a1a882bd4e36a2fc23afb3901184bd0d
ivan-burgio <ivan.24.burgio@gmail.com>
2023-10-31 11:52:00 -0300

    Cambiando la apariencia de presupuesto disponible si ya fue agotado

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -36,9 +36,9 @@ export default function ControlPresupuesto({presupuesto, gastos}) {
             <div>
                 <CircularProgressbar
                     styles = {buildStyles({
-                        pathColor: '#3B82F6',
+                        pathColor: porcentaje > 100 ? '#DC2626' : '#3B82F6',
                         trailColor: '#F5F5F5',
-                        textColor: '#3B82F6'
+                        textColor: porcentaje > 100 ? '#DC2626' : '#3B82F6'
                     })}
                     value = {porcentaje}
                     text = {`${porcentaje}% Gastado`}
@@ -50,7 +50,7 @@ export default function ControlPresupuesto({presupuesto, gastos}) {
                     <span>Presupuesto: </span> {formatearCantidad(presupuesto)}
                 </p>
 
-                <p>
+                <p className={`${disponible < 0 ? 'negativo' : ''}`}>
                     <span>Disponible: </span> {formatearCantidad(disponible)}
                 </p>
 
